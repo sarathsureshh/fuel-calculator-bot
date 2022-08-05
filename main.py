1d2ff8f135b9c3c92a519cb6cc5d9b08a09d697d
sarathsuresh <sarath.suresh@tcs.com>
2022-08-05 17:29:17 +0530

    Feature change

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
+import os
+import types
 from email.policy import default
 from imaplib import Commands
-from sqlalchemy import null
+from mimetypes import types_map
+from unittest import result
+
 import telebot
-import os
+from sqlalchemy import null
 
 API_KEY = os.getenv("API_KEY")
-mileage = 0
-distance = 0
-per_liter_cost = 0
+
 
 bot = telebot.TeleBot("5484499326:AAH2S4eEqmKACIh4v2adQU9gRhf3MLyfxhw")
 
@@ -18,9 +20,10 @@ def commandStart(message):
                                         "Say /help to see all the commands")
 
 @bot.message_handler(commands=['calculate'])
-def commandCalculate(message):    
-	bot.send_message(message.chat.id, "Alright lets calculate the amount that you need to travel!\n"
-                                      "What is your vehicle's mileage?")
+def commandCalculate(message):
+    bot.send_message(message.chat.id, "This feature is under construction, will be live soon 😉")
+	# bot.send_message(message.chat.id, "Alright lets calculate the amount that you need to travel!\n"
+    #                                   "What is your vehicle's mileage?")
                 
 @bot.message_handler(commands=['info'])
 def commandInfo(message):    
@@ -37,10 +40,11 @@ def commandHelp(message):
 @bot.message_handler(commands=['stop'])
 def commandStop(message):    
 	bot.send_message(message.chat.id, "Thanks for using me, Bye 😉")
+        
 
-    
+   
 @bot.message_handler(func=lambda query: True)
-def returnMessage(message):
+def returnMessage(message):      
     bot.send_message(message.chat.id, "Well, I'm working on myself to understand you, will learn sooner 😉")
 
 
